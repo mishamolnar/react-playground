6f827229b2ca1784d92c6c6fdf3730c923f76ce7
mmolnar <mmolnar@atlassian.com>
2022-06-28 20:38:39 +0300

    fun

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import Title from './components/Title';
+import Modal from './components/Modal';
+import EventList from './components/EventList';
+import NewEventsFrom from './components/NewEventsFrom';
+import useFetch from './hooks/useFetch';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [showEvents, setShowEvents] = useState(true);
+  const [events, setEvents] = useState([])
+  const [url, setUrl] = useState('http://localhost:3000/events')
+  const { data, isPending, error } = useFetch(url);
+
+  console.log(data)
+
+  const addEvent = (event) => {
+    setEvents((prevEvents) => {
+      return [...prevEvents, event]
+    })
+
+    setShowModal(false)
+    }
+ 
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => event.id !== id)
+    })
+    console.log(id)
+  }
+
+  const handleClose = () => {
+    console.log(showModal)
+    setShowModal(false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title="Events near you" subtitle="some events" />
+      <button onClick={() => setShowModal(true)}>show modal</button>
+      {showEvents && <div>
+        <button onClick={() => setShowEvents(false)}> hide events </button>
+      </div>}
+      {!showEvents && <div>
+        <button onClick={() => setShowEvents(true)}> show events </button>
+      </div>}
+      <div>
+        <p>Filter Events</p>
+        <button onClick={() => setUrl('http://localhost:3000/events')}>Show all</button>
+        <button onClick={() => setUrl('http://localhost:3000/events?type=birthday')}>Show birthdays</button>
+        <button onClick={() => setUrl('http://localhost:3000/events?type=funeral')}>Show funerals</button>
+      </div>
+      {isPending && <div>loading...</div>}
+      {error && <div>Something went wrong</div>}
+      {showEvents && data && <EventList events={data} handleClick={handleClick}/>}
+      {showModal && 
+        <Modal handleClose={handleClose}>
+          <NewEventsFrom addEvent={addEvent}/>
+        </Modal>}
     </div>
   );
 }
